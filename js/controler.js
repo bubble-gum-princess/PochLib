295d7b4b968a374aadc1846bc5068a4f08e3aa26
bubble-gum-princess <faiz.mouna@gmail.com>
2021-03-05 15:42:51 +0100

    librairy platforme with css styles

--- js/controler.js
+++ js/controler.js
@@ -1,65 +1,96 @@
+function hide(id) {
+    document.getElementById(id).classList.add("hidden");
+}
+function display(id) {
+    document.getElementById(id).classList.remove("hidden");
+}
+
 function addBook() {
-    document.getElementById('searchForm').classList.remove("hidden");
-    document.getElementById('newBook').classList.add("hidden");
+   display("searchForm");
+   display("line2");
+   hide("line1");
+   hide('newBook');
+  /* hide('poch-list');*/
+
 } 
 
 function cancelSearch() {
-    document.getElementById('searchForm').classList.add("hidden");
-    document.getElementById('newBook').classList.remove("hidden");
+    hide("searchForm");
+    display('newBook');
+    display('poch-list');
+    display('line1')
     document.getElementById('title').value="";
     document.getElementById('author').value="";
     document.getElementById('searchResult').innerHTML="";
-    
 }
 
 function search() {
+    document.getElementById('searchResult').innerHTML="";
     var title = document.getElementById('title').value;
     var author = document.getElementById('author').value;
+    if (!title && !author) {
+        displaySearchMessage('veuillez saisir au moins un champs');
+        return;
+    }
     var searchLink = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author + "+intitle:" + title;
+    console.log(searchLink);
     var result = httpGet(searchLink);
     
-    var books = result.items.map(item =>  {
+    var books = [];
+    var items = result.items;
+    console.log('items', items);
+    if (!items) {
+        displaySearchMessage("Aucun livre n'a était trouvé");
+        return;
+    }
+    for (var i=0; i<items.length; i++) {
+        var item = items[i];
         var volumeInfo = item.volumeInfo;
         var book = {"id" : item.id,
             "title": volumeInfo.title,
             "desc" : volumeInfo.description || 'information manquante' ,
-            "author": volumeInfo.authors[0],
+            "author": volumeInfo.authors ? volumeInfo.authors[0] : 'information manquante',
             "image": volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : 'img/unavailable.png'
         };
-        return book;   
-    });
-   
-    displayBooks(books); 
+        if (book.desc.length > 200) {
+            book.shortDesc = book.desc.substring(0,200) + '...';
+        }
+        if (book.title.length > 50) {
+            book.shortTitle = book.title.substring(0,50) + '...';
+        }
+        
+        books.push(book);
+    }
+
+    displayBooks(books);
 }
 
 function displayBooks(books) {
     var div = document.getElementById('searchResult');
-    
+    displaySearchMessage('Résultats de recherche');
     for(var i = 0; i<books.length; i++) {
         var book = books[i];
-        if (i % 2) {
-            div.innerHTML += '<div class="row">'    
+        if (i % 2 === 1) {
+            div.innerHTML += '<div class="div-empty"></div>';
         }
         div.innerHTML += bookToHtml(book);
-        if (i % 2) {
-            div.innerHTML += '</div>'    
-        }
     }
-
     
     books.forEach(book => console.log(book));
 }
 
+function displaySearchMessage(msg) {
+    document.getElementById('searchResult').innerHTML += '<div class="text-center"> <h5>' + msg + '</h5> </div>';
+}
+
 function bookToHtml(book) {
-    return '<div class="col-lg-1"></div>'
-    + '<div class="col-lg-4 div-book">'
-+ '<h2>Titre: ' + book.title + '</h2>'
-+ '<h2>Id: '+ book.id + '</h2>'
-+ '<h4>Auteur: ' + book.author + '</h4>'
-+ '<p>Description: '+ book.desc + '</p>'
-+ '<img src="'+ book.image + '" class="img-responsive img-book" >'
-+ '</div>'
-    + '<div class="col-lg-1"></div>';
+    return '<div class="div-book">'
+    + '<h4>Titre: ' + (book.shortTitle || book.title) + '</h4>'
+    + '<h5>Id: '+ book.id + '</h5>'
+    + '<h5>Auteur: ' + book.author + '</h5>'
+    + '<p>Description: '+ (book.shortDesc || book.desc) + '</p>'
+    + '<img src="'+ book.image + '" class="img-responsive img-book" >'
+    + '</div>';
 }
 
 function httpGet(theUrl) {
